dfdbe6538a4ce37ebe3f6686a6168313a1c1ec16
pokotsun <gorigolila@gmail.com>
2023-06-26 20:46:33 +0900

    サーバー側で cors が発生しないようにした

--- server.go
+++ server.go
@@ -1,14 +1,21 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPost, http.MethodDelete},
+	}))
+	e.GET("/hello", func(c echo.Context) error {
+		c.Response().Header().Set(echo.HeaderAccessControlAllowOrigin, "*")
+		return c.JSON(http.StatusOK, "{ 'status': 'Hello, World!' }")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
