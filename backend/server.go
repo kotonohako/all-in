5af4d73ecd11fa848cc72f0e2ec045ae66ce9a04
pokotsun <gorigolila@gmail.com>
2023-06-28 01:07:32 +0900

    自動生成コードを呼ぶように server.go を修正

--- backend/server.go
+++ backend/server.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	ga "kotonohako-backend/presentation/generated/api"
+	gt "kotonohako-backend/presentation/generated/types"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Registry struct{}
+
+func (r Registry) Health(ctx echo.Context) error {
+	status := "200 ok"
+	helloResponse := gt.HelloResponse{Status: &status}
+	return ctx.JSON(http.StatusOK, helloResponse)
+}
+
 func main() {
 	e := echo.New()
+	r := Registry{}
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPost, http.MethodDelete},
 	}))
-	e.GET("/hello", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, "{ 'status': 'Hello, Kotonohako World!' }")
-	})
+	ga.RegisterHandlers(e, r)
 	e.Logger.Fatal(e.Start(":8080"))
 }
