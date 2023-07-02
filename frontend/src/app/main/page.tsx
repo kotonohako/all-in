de47826c131d7816c610767bdada09ec1a9af275
tacptac <tacptac.appdev@gmail.com>
2023-07-02 20:51:41 +0900

    mainページを追加

---
+++ frontend/src/app/main/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import styles from './page.module.css'
+
+export default function About() {
+    return (
+        <main className={styles.main}>
+            hogehoge
+        </main>
+    )
+}
