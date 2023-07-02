82ca2729b3604b6b13355e861a0ac2d9afc3dd0b
pokotsun <gorigolila@gmail.com>
2023-07-02 22:15:22 +0900

    API の参照先を修正

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // GETリクエスト
 export const getJSON = async (): Promise<any> => {
-  const url = "http://localhost:8080/v1/health";
+  const url = "http://localhost:8080/v1/kotonohas";
   const response = await axios.get(url);
   return response.data;
 };
