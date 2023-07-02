892be8b9c8da2461a91ea36c586690a678a2f651
pokotsun <gorigolila@gmail.com>
2023-07-02 21:56:06 +0900

    API の参照先を修正して、一覧を返すようにした

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -4,7 +4,7 @@ import { getJSON } from '../services/api'
 import useSWR from "swr"
 
 export default function About() {
-    const { data, error } = useSWR("http://localhost:8080/hello", getJSON)
+    const { data, error } = useSWR("http://localhost:8080/v1/kotonohas", getJSON)
     return (
         <main className={styles.main}>
             <div>{JSON.stringify(data)}</div>
