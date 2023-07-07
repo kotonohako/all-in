d43c9c0605f0bcaecbc5bd644c44c184561cfa71
pokotsun <gorigolila@gmail.com>
2023-07-07 23:01:01 +0900

    自動生成系のファイルを generated 直下に置くようにした

--- backend/generate-from-yaml.sh
+++ backend/generate-from-yaml.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-oapi-codegen -generate types -package generated ../api.yaml > presentation/generated/types/types.gen.go
-oapi-codegen -generate server -package generated ../api.yaml > presentation/generated/api/api.gen.go
+oapi-codegen -generate types -package generated ../api.yaml > presentation/generated/types.gen.go
+oapi-codegen -generate server -package generated ../api.yaml > presentation/generated/api.gen.go
 
