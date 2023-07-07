3d5061d6dba254ff5dd937440d2d670616d1b53c
pokotsun <gorigolila@gmail.com>
2023-07-07 23:06:12 +0900

    registry を作成

--- backend/registry/registry.go
+++ backend/registry/registry.go
@@ -3,6 +3,7 @@ package registry
 import (
 	"net/http"
 
+	"github.com/kotonohako/all-in/backend/presentation/generated"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,14 +11,14 @@ type ApiRegistry struct{}
 
 func (r ApiRegistry) Health(ctx echo.Context) error {
 	status := "200 ok"
-	helloResponse := HelloResponse{Status: &status}
+	helloResponse := generated.HelloResponse{Status: &status}
 	return ctx.JSON(http.StatusOK, helloResponse)
 }
 
 func (r ApiRegistry) API(ctx echo.Context) error {
 	quoteMediaType := "本"
 	quoteSourceName := "ノルウェイの森"
-	kotonohasResponse := KotonohaResponse{
+	kotonohasResponse := generated.KotonohaResponse{
 		Author:          "村上春樹",
 		KotonohaId:      12345,
 		QuoteMediaType:  &quoteMediaType,
--- backend/server.go
+++ backend/server.go
@@ -4,38 +4,18 @@ import (
 	"net/http"
 
 	"github.com/kotonohako/all-in/backend/presentation/generated"
+	"github.com/kotonohako/all-in/backend/registry"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-type Registry struct{}
-
-func (r Registry) Health(ctx echo.Context) error {
-	status := "200 ok"
-	helloResponse := generated.HelloResponse{Status: &status}
-	return ctx.JSON(http.StatusOK, helloResponse)
-}
-
-func (r Registry) API(ctx echo.Context) error {
-	quoteMediaType := "本"
-	quoteSourceName := "ノルウェイの森"
-	kotonohasResponse := generated.KotonohaResponse{
-		Author:          "村上春樹",
-		KotonohaId:      12345,
-		QuoteMediaType:  &quoteMediaType,
-		QuoteSourceName: &quoteSourceName,
-		Sentence:        "希望があるところには必ず試練があるものだから",
-	}
-	return ctx.JSON(http.StatusOK, kotonohasResponse)
-}
-
 func main() {
 	e := echo.New()
-	r := Registry{}
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPost, http.MethodDelete},
 	}))
+	r := registry.ApiRegistry{}
 	generated.RegisterHandlers(e, r)
 	e.Logger.Fatal(e.Start(":8080"))
 }
