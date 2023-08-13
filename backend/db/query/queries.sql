db698d13f97e75b1cb7510db5b63cebc1527dca3
pokotsun <gorigolila@gmail.com>
2023-08-13 16:29:42 +0900

    queries.sql に INSERT 系の処理追加

--- backend/db/query/queries.sql
+++ backend/db/query/queries.sql
@@ -3,3 +3,14 @@ SELECT * FROM quote;
 
 -- name: GetQuote :one
 SELECT * FROM quote WHERE id = ?;
+
+-- name: CreateQuote :exec
+INSERT INTO quote (
+    sentence, 
+    speaker_name, 
+    quote_source_name, 
+    quote_media_type
+)VALUES (?, ?, ?, ?);
+
+-- name: GetLastInsertId :one
+SELECT LAST_INSERT_ID();
